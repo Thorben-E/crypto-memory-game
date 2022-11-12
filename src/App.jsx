d70cb576bbce13e78b971e020e0d55025137ce83
Thorben <thorbenegberink@gmail.com>
2022-11-12 23:12:51 +0000

    added display of cards, handleOnClick, shuffle and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,133 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import bitcoin from './assets/bitcoin.png'
+import bnb from './assets/bnb.png'
+import cardano from './assets/cardano.png'
+import dai from './assets/dai.png'
+import doge from './assets/dogecoin.png'
+import eth from './assets/ethereum.png'
+import polkadot from './assets/polkadot.png'
+import polygon from './assets/polygon.png'
+import tether from './assets/tether.svg'
+import xrp from './assets/xrp.png'
+import sol from './assets/solana.png'
+import ltc from './assets/litecoin.png'
 
 function App() {
 
   const [score, setScore] = useState(0)
+  const [SelectedArray, setSelectedArray] = useState([])
+  const [bestScore, setBestScore] = useState(0)
   const [cardArray, setCardArray] = useState([
     {
-      name: 'Bitcoin',
-      img: './assets/bitcoin.png'
+      img: bitcoin,
+      name: 'Bitcoin'
     },
     {
-      name: 'Ethereum',
-      img: './assets/ethereum.png'
+      img: eth,
+      name: 'Ethereum'
     },
     {
-      name: 'Bnb',
-      img: './assets/bnb.png'
+      img: bnb,
+      name: 'Bnb'
     },
     {
-      name: 'Cardana',
-      img: './assets/cardano.png' 
+      img: cardano,
+      name: 'Cardano' 
     },
     {
-      name: 'Dai',
-      img: './assets/dogecoin.png' 
+      img: dai,
+      name: 'Dai' 
     },
     {
-      name: 'Polkadot',
-      img: './assets/polkadot.png'
+      img: polkadot,
+      name: 'Polkadot'
     },
     {
-      name: 'Polygon',
-      img: './assets/polygon.png'
+      img: polygon,
+      name: 'Polygon'
     },
     {
-      name: 'Tether',
-      img: './assets/tether.svg' 
+      img: tether,
+      name: 'Tether' 
     },
     {
-      name: 'Xrp',
-      img: './assets/xrp.png'
+      img: xrp,
+      name: 'Xrp'
     },
     {
-      name: 'Dogecoin',
-      img: './assets/dogecoin.png'
+      img: doge,
+      name: 'Doge'
+    },
+    {
+      img: ltc,
+      name: 'Litecoin'
+    },
+    {
+      img: sol,
+      name: 'Solana'
     }
   ])
+
+  const handleOnClick = (e) => {
+    //chech for GAMEOVER
+    SelectedArray.forEach(item => {
+      // if card === item - game over
+      if (item === e.target.id) {
+        console.log('gameover')
+        setScore(0)
+        setSelectedArray([])
+      }
+    })
+    //save card in selected array
+    setSelectedArray(current => [...current, e.target.id])
+
+    //shuffle cardArray
+    setCardArray(shuffle(cardArray))
+
+    //score + 1
+    setScore(score+1)
+  }
+
+  useEffect(() => {
+    if (score > bestScore) {
+      setBestScore(score)
+    }
+  })
+
+  const GAMEOVER = () => {
+    
+  }
+
+  const shuffle = (array) => {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
 
   return (
     <div className="App">
       <h2>Crypto memory game</h2>
       <p>Score: {score}</p>
+      <p>Best score: {bestScore}</p>
+      <ul>{cardArray.map((card) => (
+        <li key={card.name} id={card.name} className="card" onClick={handleOnClick}>
+          <p id={card.name}>{card.name}</p>
+          <img src={card.img} id={card.name} className="logo" alt="it did no load :(" />
+        </li>
+      ))}</ul>
     </div>
   )
 }
